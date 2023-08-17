6f4c05f9d91f44f7df6cf53dc06a75b6a5fd9c01
Akaabiam Richards <richardsakaabiam@gmail.com>
2023-08-17 16:15:31 +0100

    updated

--- src/main/java/com/richards/loginAuthentication/controller/UserController.java
+++ src/main/java/com/richards/loginAuthentication/controller/UserController.java
@@ -6,14 +6,12 @@ import com.richards.loginAuthentication.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 
 public class UserController {
 
