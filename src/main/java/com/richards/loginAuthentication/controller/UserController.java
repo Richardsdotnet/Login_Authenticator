be3ad3440d0d127ae565fc7c69d8b7ad890be96d
Akaabiam Richards <richardsakaabiam@gmail.com>
2023-08-18 01:13:16 +0100

    updated

--- src/main/java/com/richards/loginAuthentication/controller/UserController.java
+++ src/main/java/com/richards/loginAuthentication/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.richards.loginAuthentication.controller;
 
+import com.richards.loginAuthentication.dto.requests.LoginRequest;
 import com.richards.loginAuthentication.dto.requests.RegistrationRequest;
+import com.richards.loginAuthentication.dto.response.LoginResponse;
 import com.richards.loginAuthentication.dto.response.RegistrationResponse;
+import com.richards.loginAuthentication.exceptions.BadCredentialsExceptions;
 import com.richards.loginAuthentication.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,10 +23,16 @@ public class UserController {
 
         private final UserService userServices;
 
-        @PostMapping
+        @PostMapping("/register")
         public ResponseEntity<RegistrationResponse> register(@RequestBody RegistrationRequest registerUserRequest) {
-            var response = userServices.register(registerUserRequest);
+            RegistrationResponse response = userServices.register(registerUserRequest);
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        }
+
+        @PostMapping("/login")
+        public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) throws BadCredentialsExceptions {
+            LoginResponse response = userServices.login(loginRequest);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
         }
 
     }
